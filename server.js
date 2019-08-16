78cfb1d8a964f36997bc2f5e177fe4021e2a0c71
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-16 10:29:05 -0700

    001-projects.js seed coded. About to run seeds

--- server.js
+++ server.js
@@ -12,4 +12,20 @@ server.get('/', (req, res) => {
 	res.send(`<h1>API is running</h1>`);
 });
 
+// CRUD
+// GET
+// get all projects
+server.get('/projects', async (req, res) => {
+	try {
+		const projects = await db('projects');
+		res.status(200).json(projects);
+	}
+	catch(error) {
+		res.status(500).json({message: 'error retrieving projects'});
+	}
+});
+
+
+// POST
+
 module.exports = server;
