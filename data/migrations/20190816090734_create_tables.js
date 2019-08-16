2e0e29b1cd63d24e9d95eb245bca54c33e1b519e
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-16 10:02:47 -0700

    Create_tables.js schema laid out. About to make migration

--- data/migrations/20190816090734_create_tables.js
+++ data/migrations/20190816090734_create_tables.js
@@ -2,10 +2,53 @@
 exports.up = function(knex) {
 	return knex.schema
 	// ONE project can have MANY tasks
+		.createTable('projects', tbl => {
+			tbl.increments('project_id');
 
-	// ONE task belongs to only ONE project
+			tbl
+				.string('project_name', 128)
+				.notNullable()
+				.unique()
+			tbl
+				.string('project_description', 255)
+			tbl
+				.boolean('project_completed').notNullable().defaultTo(false)
+		})
 
 	// ONE project can have MANY resources
+		.createTable('resources', tbl => {
+			tbl.increments('resource_id');
+
+			tbl
+				.string('resource_name', 128)
+					.notNullable()
+					.unique()
+			tbl
+				.string('resource_description', 255)
+		})
+
+	// ONE task belongs to only ONE project
+		.createTable('tasks', tbl => {
+			tbl.increments('task_id');
+
+			tbl
+				.string('task_description', 255)
+				.notNullable()
+			tbl
+				.text('task_notes')
+			tbl
+				.boolean('task_completed').notNullable().defaultTo(false)
+
+			// Foreign Key
+			tbl
+				.integer('project_id')
+				.unsigned()
+				.notNullable()
+				.references('project_id')
+				.inTable('projects')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE')
+		})
 
 	// ONE resource can be used in MANY project
 };
