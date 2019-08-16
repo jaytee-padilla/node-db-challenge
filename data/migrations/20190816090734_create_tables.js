6e824eabd1ac88e468e8a2abe04e7219c85fa21c
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-16 10:06:15 -0700

    Filled out exports.down in create_tables.js. About to make migration

--- data/migrations/20190816090734_create_tables.js
+++ data/migrations/20190816090734_create_tables.js
@@ -54,5 +54,8 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema
+	return knex.schema
+		.dropTableIfExists('tasks')
+		.dropTableIfExists('resources')
+		.dropTableIfExists('projects')
 };
